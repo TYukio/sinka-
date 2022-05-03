366db6373fccf65f6d0942ab64191dbe85b52502
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-05-03 10:29:50 -0300

    Node ajustado para trabalhar com router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET react router */
+router.get('*', function(req, res, next) {
   res.sendFile(path.join(process.env.CLIENT_DIST, 'index.html'))
 });
 
