e3631ffcf879ebeee24986c0a032a3ba1588dd2a
Nicolas Brun <nicolaabrun@gmail.com>
2022-05-25 11:48:49 -0300

    Mudanças Dashboard(Requer Responsividade)
    Mudanças no perfil e sidebar do dashboard.

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,7 +1,8 @@
 import { useState, useContext } from 'react';
-import { Stack, Collapse, Typography, Divider, Button, IconButton, useTheme, useMediaQuery, Box } from '@mui/material';
-import { Home, Person, Logout, Login, DoubleArrowRounded } from '@mui/icons-material';
+import { Stack, Collapse, Typography, Divider, Button, IconButton, useTheme, useMediaQuery, Box, Icon } from '@mui/material';
+import { Home, HomeOutlined, Person, PersonOutline, Logout, Login, DoubleArrowRounded, GroupsOutlined, DashboardOutlined } from '@mui/icons-material';
 import { SessionContext } from '../../util/contexts';
+import logo from './sinka.svg';
 
 function Dashboard(props) {
     const theme = useTheme();
@@ -9,65 +10,67 @@ function Dashboard(props) {
     const [expanded, setExpanded] = useState(false);
 
     const session_uid = useContext(SessionContext);
+    
 
     const additionalbuttons = Array.isArray(props.buttons) ? props.buttons : [];
     const defaultbuttons = [
-        {label: 'home', icon: <Home />, href: '/'},
-    ]
-    
-    if (session_uid !== null)
-    {
-        defaultbuttons.push({label: 'meu perfil', icon: <Person />, href: '/user/' + session_uid});
-    }
+        { label: 'Home', icon: <HomeOutlined />, href: '/' },
+        { label: 'Perfil', icon: <PersonOutline />, href: '/' },
+        { label: 'Dashboard', icon: <DashboardOutlined/>, href: '/' },
         
-    const dashbuttons = props.useDefault === true ?  defaultbuttons.concat(additionalbuttons) : additionalbuttons;
+        { label: 'Meu Time', icon: <GroupsOutlined/>, href: '/' },
+    ]
+
+    if (session_uid !== null) {
+        defaultbuttons.push({ label: 'meu perfil', icon: <Person />, href: '/user/' + session_uid });
+    }
+
+    const dashbuttons = props.useDefault === true ? defaultbuttons.concat(additionalbuttons) : additionalbuttons;
 
     return (
-        <Box sx={{flexShrink: 0, position: 'relative', width: mobile ? 'none' : '14rem'}}>
-            <Stack direction="row" sx={{position: 'absolute', zIndex: 256, height:'100%', minHeight: '100vh'}}>  
+        <Box sx={{ flexShrink: 0, position: 'relative', width: mobile ? 'none' : '14rem' }}>
+            <Stack direction="row" sx={{ position: 'absolute', zIndex: 256, height: '100%', minHeight: '100vh' }}>
                 <Collapse in={expanded || !mobile} orientation="horizontal">
-                    <Stack direction="column" sx={{backgroundColor: theme.palette.background.overlay, height: '100%', width: '14rem', alignItems: 'center', justifyContent: 'flex-start'}} spacing={2}>
-                        <Typography fontWeight="bold" variant="h6" letterSpacing={'0.1rem'} component="div" sx={{marginBottom: '-1.5rem'}}>
-                            <p>DASHBOARD</p>
-                        </Typography>
+                    <Stack direction="column" sx={{ backgroundColor: theme.palette.background.overlay, height: '100%', width: '14rem', justifyContent: 'flex-start' }} spacing={2}>
+                        <Box sx={{height:'2.8em', marginTop:'1em', textAlign: 'center'}}><img src={logo} style={{ height: '100%' }} ></img></Box>
 
-                        <Divider sx={{width: '80%'}} orientation="horizontal" />
+                        <Divider textAlign="left" sx={{ flexGrow: 0, alignContent: 'center', padding: '-2em' }} />
                         {
-                            dashbuttons.map((btn, i) =>
-                            {
+                            dashbuttons.map((btn, i) => {
                                 if (btn.href == null)
                                     return (
                                         <Box width="80%" display="inline-flex" alignItems="center">
-                                            <Typography sx={{marginRight: '1em', opacity: '75%', textTransform: 'upperCase', fontSize: '0.8em'}}>
+                                            <Typography sx={{ marginRight: '1em', opacity: '75%', textTransform: 'upperCase', fontSize: '0.8em' }}>
                                                 {btn.label}
                                             </Typography>
-                                            <Divider textAlign="left" sx={{flexGrow: 1, alignContent: 'center', padding: '-2em'}}/>
+                                            <Divider textAlign="left" sx={{ flexGrow: 1, alignContent: 'center', padding: '-2em' }} />
                                         </Box>
 
                                     );
                                 else
-                                    return(
-                                        <Button href={btn.href} color="neutral" sx={{width: '80%', height: '3rem', my: '-2em'}} startIcon={btn.icon}>
+                                    return (
+                                        <Button href={btn.href} color="neutral" sx={{ width: '100%', height: '2.5rem', my: '-2em', justifyContent: 'flex-start', paddingLeft: '1.5em' }} startIcon={btn.icon}>
                                             {btn.label}
                                         </Button>
                                     );
                             })
                         }
+                        <Divider textAlign="left" sx={{ flexGrow: 0, alignContent: 'center', padding: '-2em' }} />
 
-                        <Stack direction="column-reverse" sx={{pb: '1em', flexGrow: 1, alignItems: 'center', width: '100%'}}>
+                        <Stack direction="column-reverse" sx={{ pb: '1em', flexGrow: 1, alignItems: 'center', width: '100%' }}>
 
-                            <Button href={session_uid !== null ? '/auth/signout' : '/entrar'} color="neutral" sx={{width: '80%', height: '3rem'}} startIcon={session_uid !== null ? <Logout color="error" /> : <Login color="success" />}>
+                            <Button href={session_uid !== null ? '/auth/signout' : '/entrar'} color="neutral" sx={{ width: '100%', height: '3rem' }} startIcon={session_uid !== null ? <Logout color="error" /> : <Login color="success" />}>
                                 {session_uid !== null ? 'Sair' : 'Entrar'}
                             </Button>
 
                         </Stack>
 
                     </Stack>
-                    
+
                 </Collapse>
 
-                <IconButton disableRipple={true} aria-label="dashboard" size="large" onClick={() => setExpanded(!expanded)} sx={{borderRadius: 0, display: mobile ? 'auto' : 'none'}}>
-                    <DoubleArrowRounded sx={{transition: '0.25s', transform: expanded ? 'rotate(-180deg)': 'none'}} />
+                <IconButton disableRipple={true} aria-label="dashboard" size="large" onClick={() => setExpanded(!expanded)} sx={{ borderRadius: 0, display: mobile ? 'auto' : 'none' }}>
+                    <DoubleArrowRounded sx={{ transition: '0.25s', transform: expanded ? 'rotate(-180deg)' : 'none' }} />
                 </IconButton>
             </Stack>
         </Box>
--- frontend/src/pages/profiles/User.js
+++ frontend/src/pages/profiles/User.js
@@ -1,12 +1,13 @@
 import { useEffect, useState, useContext } from 'react';
-import { useTheme, Box, Stack, Avatar, Typography, Divider, Fab, Chip, Icon } from '@mui/material';
+import { useTheme, Box, Stack, Avatar, Typography, Divider, Fab, Chip, Icon, Grid } from '@mui/material';
 import { CalendarMonth, Edit } from '@mui/icons-material';
 import { useParams, useNavigate } from 'react-router-dom';
 import { SessionContext, HostContext } from '../../util/contexts';
+import VerifiedIcon from '@mui/icons-material/Verified';
 import Dashboard from '../../components/dashboard/Dashboard';
 import Loading from '../../components/Loading';
 
-import defaultbanner from './defaultbanner.jpg'
+import defaultbanner from './defaultbanner.png'
 
 function User(props) {
 	const [userdata, setUserdata] = useState();
@@ -20,95 +21,118 @@ function User(props) {
 	const [userTypes, setUserTypes] = useState([]);
 	const [sports, setSports] = useState([]);
 
-    // Backend
+	// Backend
 	const hostname = useContext(HostContext);
 
-    function fetchUserdata()
-    {
+	function fetchUserdata() {
 		if (profile_uid === null)
 			return;
 
-        fetch(hostname + 'userdata/fetchpublic?id=' + profile_uid, {
-            method: 'GET'
-        }).then(response => {
-            if (response.ok) { 
-                response.json().then((json) => setUserdata(json));
-            } else if (response.status === 404) {
-                navigate('/user');
-            } 
-        });
-    }
+		fetch(hostname + 'userdata/fetchpublic?id=' + profile_uid, {
+			method: 'GET'
+		}).then(response => {
+			if (response.ok) {
+				response.json().then((json) => setUserdata(json));
+			} else if (response.status === 404) {
+				navigate('/user');
+			}
+		});
+	}
 
-	function fetchDatafields()
-    {
-        fetch(hostname + 'datafields/usertypes', {
-            method: 'GET'
-        }).then(response => {
-            if (response.ok) { 
-                response.json().then((json) => {
-                    setUserTypes(json);
-                });
-            }
-        });
+	function fetchDatafields() {
+		fetch(hostname + 'datafields/usertypes', {
+			method: 'GET'
+		}).then(response => {
+			if (response.ok) {
+				response.json().then((json) => {
+					setUserTypes(json);
+				});
+			}
+		});
 
 		fetch(hostname + 'datafields/sports', {
-            method: 'GET'
-        }).then(response => {
-            if (response.ok) { 
-                response.json().then((json) => {
-                    setSports(json);
-                });
-            }
-        });
-    }
+			method: 'GET'
+		}).then(response => {
+			if (response.ok) {
+				response.json().then((json) => {
+					setSports(json);
+				});
+			}
+		});
+	}
 
 	/*eslint-disable */
-	useEffect(() => {fetchUserdata(); fetchDatafields();}, []);
+	useEffect(() => { fetchUserdata(); fetchDatafields(); }, []);
 	/*eslint-enable */
 
-	if (userdata)
-	{
+	if (userdata) {
 		return (
 			<Box height="100%" width="100%" display="flex">
 				<Dashboard useDefault={true} />
 
-				<Stack direction="column" sx={{padding: '2rem', paddingTop: '0', flexGrow: 1, overflowWrap: 'break-all', alignItems: 'center' }}>
-					
-					<Box sx={{backgroundImage: `url(${defaultbanner})`, maxWidth: '1920px', width: 'calc(100% + 4rem)', marginX: '-4rem',
-						alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-						<Avatar src={hostname + `images/pfp/${profile_uid}.jpg?${new Date().valueOf()}`} sx={{ width: '6em', height: '6em', marginY: '0.25em', border: '0.12em solid', borderColor: theme.palette.common.white }}/>
+				<Stack direction="column" sx={{ padding: '2rem', paddingTop: '0', flexGrow: 1, overflowWrap: 'break-all', alignItems: 'center' }}>
 
-						<Typography fontWeight="bold" variant="h4" component="div" sx={{marginY: '-2rem', color: theme.palette.common.white}}>
+					<Box sx={{
+						backgroundImage: `url(${defaultbanner})`, maxWidth: '4920px', width: 'calc(100% + 4rem)', marginX: '-4rem', height: '30vh',
+						alignItems: 'center', display: 'flex', flexDirection: 'column'
+					}}>
+					</Box>
+					<Box sx={{
+						display: 'flex',
+						height: '20vh',
+						marginLeft: '-40em',
+						gap: '1.5em',
+						justifyContent: 'space-between'
+					}}>
+
+						<Avatar src={hostname + `images/pfp/${profile_uid}.jpg?${new Date().valueOf()}`} sx={{ width: '6.5em', height: '6.5em', marginY: '0.25em', border: '0.12em solid', borderColor: theme.palette.common.white, marginTop: '-1.5em', }} />
+
+						<Typography fontWeight="500" variant="h4" component="div" sx={{ marginY: '-1em', color: theme.palette.common.white, marginTop: '-15px' }}>
 							<p>{userdata.full_name}</p>
+							<Box sx={{ display: 'flex', flexDirection: 'row', fontSize: '0.725rem', alignItems: 'center', letterSpacing: '0.06em', marginTop: '-3.5em', marginLeft: '5px', }}>
+								<p>Membro desde</p>
+								<Typography fontWeight="bold" component="div" sx={{ marginLeft: '0.35em', fontSize: '0.725rem', marginTop:'1px', }}>
+									<p>{userdata.creation.substr(0, 10).split('-').reverse().join('/')}</p>
+
+								</Typography>
+
+
+
+							</Box>
 						</Typography>
+						<Box sx={{
+							marginLeft: '-1em',
+							marginTop: '1.8em',
+						}}>
+							<VerifiedIcon sx={{
+								color: 'cyan',
+							}}></VerifiedIcon>
+
+						</Box>
+
 					</Box>
 
-					<CalendarMonth sx={{margin: '1rem 0 -0.5rem 0'}}/>
-					<Box sx={{display: 'flex', flexDirection: 'row', fontSize: '1.125rem', alignItems: 'center', letterSpacing: '0.06em'}}>
-						<p>Membro desde</p>
-						<Typography fontWeight="bold" component="div" sx={{marginLeft: '0.35em'}}>
-							<p>{userdata.creation.substr(0, 10).split('-').reverse().join('/')}</p>
-						</Typography>
-					</Box>	
 
-					<Box sx={{display: 'flex', gap: '0.35em', flexDirection: 'row', fontSize: '1.125rem', alignItems: 'center', letterSpacing: '0.06em', mb: '1em'}}>
-						
+
+
+					<Box sx={{ display: 'flex', gap: '0.35em', flexDirection: 'row', fontSize: '1.125rem', alignItems: 'center', letterSpacing: '0.06em', mb: '1em' }}>
+
 						{Object.keys(userdata.types).map((key) => {
 							if (userTypes === null || userTypes === undefined || userTypes.length === 0)
-								return(undefined);
+								return (undefined);
 							const curtype = userTypes.find(element => element.id === userdata.types[key]);
 							return (
 								<Chip icon={<Icon>{curtype.mui_icon}</Icon>} label={curtype.title} />
 							);
 						})}
 					</Box>
-					<Divider sx={{width: '16rem'}} />
+					<Divider sx={{ width: '16rem' }} />
 
 					<Typography fontSize="1.125rem" component="div" letterSpacing="0.06em">
 						<p>Sobre</p>
 					</Typography>
 
-					<Typography component="div" sx={{borderRadius: '0.5em', textAlign: 'center', minWidth: '20rem', width: '50%', backgroundColor: theme.palette.background.overlay, padding: '0em 1em'}}>
+					<Typography component="div" sx={{ borderRadius: '0.5em', textAlign: 'center', minWidth: '20rem', width: '50%', backgroundColor: theme.palette.background.overlay, padding: '0em 1em' }}>
 						<p>{userdata.biography !== null ? userdata.biography : 'Este usuário não adicionou sua biografia'}</p>
 					</Typography>
 
@@ -116,15 +140,15 @@ function User(props) {
 						<p>Esportes</p>
 					</Typography>
 
-					<Typography sx={{marginTop: '-1em', textAlign: 'center', display: (userdata.sports && userdata.sports.length > 0) ? 'none' : 'auto'}} fontSize="0.965rem" component="div" letterSpacing="0.02em">
+					<Typography sx={{ marginTop: '-1em', textAlign: 'center', display: (userdata.sports && userdata.sports.length > 0) ? 'none' : 'auto' }} fontSize="0.965rem" component="div" letterSpacing="0.02em">
 						<p>Nenhum esporte por aqui</p>
 					</Typography>
 
 
-					<Box sx={{width: '60%', minWidth: '18em', display: 'flex', gap: '0.35em', flexDirection: 'row', flexWrap: 'wrap', fontSize: '1.125rem', justifyContent: 'center', letterSpacing: '0.06em', mb: '1em'}}>
+					<Box sx={{ width: '60%', minWidth: '18em', display: 'flex', gap: '0.35em', flexDirection: 'row', flexWrap: 'wrap', fontSize: '1.125rem', justifyContent: 'center', letterSpacing: '0.06em', mb: '1em' }}>
 						{Object.keys(userdata.sports).map((key) => {
 							if (sports === null || sports === undefined || sports.length === 0)
-								return(undefined);
+								return (undefined);
 							const curtype = sports.find(element => element.id === userdata.sports[key]);
 							return (
 								<Chip color="primary" icon={<Icon>{curtype.mui_icon}</Icon>} label={curtype.title} />
@@ -132,8 +156,10 @@ function User(props) {
 						})}
 					</Box>
 
-					<Fab href="/editarperfil" color="primary" aria-label="edit" sx={{position: 'absolute', bottom: '3rem', right: '3rem', zIndex: 255, 
-						display: session_uid === parseInt(profile_uid) ? 'auto' : 'none'}}>
+					<Fab href="/editarperfil" color="primary" aria-label="edit" sx={{
+						position: 'absolute', bottom: '3rem', right: '3rem', zIndex: 255,
+						display: session_uid === parseInt(profile_uid) ? 'auto' : 'none'
+					}}>
 						<Edit />
 					</Fab>
 
